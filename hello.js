25526cc9b8c25d9ae88f20dd09dc809c5e2224c4
Isabelinhh <carmo.isabelly020@gmail.com>
2025-10-07 15:26:31 -0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+// variavel com seu nome
+const meuNome = "isa";
+// função qu exibe uma menssagem de boas vindas
+function exibirBoasVindas() {
+    //use console.log para exibir  "Olá, [seu nome"]! Bem-vindo ao node.js
+   console.log("Olá",meuNome,"!" ,"Bem-vindo ao node.js");
+}
+exibirBoasVindas();
+console.log(process.version);
+
+
